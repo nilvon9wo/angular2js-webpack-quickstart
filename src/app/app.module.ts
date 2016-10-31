95472d2e3345d2c641e35638a91ff42a22e2b9ec
Brian <kessler.bm@gmail.com>
2016-10-31 15:26:18 +0100

    moved hero-details into separate component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { TourOfHeroesComponent } from './tour-of-heroes/tour-of-heroes.component';
+import { HeroDetailComponent } from './tour-of-heroes/hero-detail/hero-detail.component';
 
 @NgModule({
     bootstrap: [ TourOfHeroesComponent ],
-    declarations: [ TourOfHeroesComponent ],
+    declarations: [ TourOfHeroesComponent, HeroDetailComponent ],
     imports: [ BrowserModule, FormsModule ]
 })
 
---
+++ src/app/tour-of-heroes/hero-detail/hero-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+import { Hero } from '../hero';
+
+@Component({
+    selector: 'hero-detail',
+    template: require( './hero-detail.component.html' )
+})
+
+export class HeroDetailComponent {
+    @Input()
+    public hero: Hero;
+}
