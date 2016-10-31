a333c6497f5cc2c480f267018cf2136d7d9db4e8
Brian <kessler.bm@gmail.com>
2016-10-31 16:14:01 +0100

    add hero dao service and mocked out the response

---
+++ src/app/tour-of-heroes/hero/hero-dao.service.ts
@@ -0,0 +1,10 @@
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class HeroDaoService {
+    public getHeroes(): Promise<Hero[]> {
+        return Promise.resolve(HEROES);
+    }
+}
