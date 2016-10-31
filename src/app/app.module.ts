65e9817ac0e35a99b5d06a8b107b334185a4d2b1
Brian <kessler.bm@gmail.com>
2016-10-31 12:14:08 +0100

    Added 2-way binding to Hero name

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
-import { TourOfHeroesComponent } from './tour-of-heroes/tour-of-heroes.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
+
+import { TourOfHeroesComponent } from './tour-of-heroes/tour-of-heroes.component';
 
 @NgModule({
     bootstrap: [ TourOfHeroesComponent ],
     declarations: [ TourOfHeroesComponent ],
-    imports: [ BrowserModule ]
+    imports: [ BrowserModule, FormsModule ]
 })
 
 export class AppModule {}
