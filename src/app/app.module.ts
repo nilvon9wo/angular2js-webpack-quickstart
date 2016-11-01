450bed5f18c9cfa53213ec0fb583e37f2dbc1576
Brian <kessler.bm@gmail.com>
2016-11-01 02:13:08 +0100

    added heroes route to hero-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
-import { TourOfHeroesComponent } from './tour-of-heroes/tour-of-heroes.component';
 import { HeroDetailComponent } from './tour-of-heroes/hero-detail/hero-detail.component';
+import { HeroDaoService } from './tour-of-heroes/hero-dao/hero-dao.service';
 import { HeroListComponent } from './tour-of-heroes/hero-list/hero-list.component';
+import { TourOfHeroesComponent } from './tour-of-heroes/tour-of-heroes.component';
 
 @NgModule({
     bootstrap: [ TourOfHeroesComponent ],
-    declarations: [ TourOfHeroesComponent, HeroListComponent, HeroDetailComponent ],
-    imports: [ BrowserModule, FormsModule ]
+    declarations: [ HeroDetailComponent, HeroListComponent, TourOfHeroesComponent ],
+    imports: [
+              BrowserModule,
+              FormsModule,
+              RouterModule.forRoot([{
+                  path: 'heroes',
+                  component: HeroListComponent
+                }])
+            ],
+    providers: [ HeroDaoService ]
 })
 
 export class AppModule {}
