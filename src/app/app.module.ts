62eee9734b3d426b4860267c173319d5b4c819cf
Brian <kessler.bm@gmail.com>
2016-11-01 16:59:36 +0100

    added search function to find heros

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { HttpModule } from '@angular/http';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import './rxjs-extensions';
 
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryHeroDataService } from './tour-of-heroes/heroes/hero-data/in-memory-hero-data.service';
@@ -12,11 +13,12 @@ import { DashboardComponent } from './tour-of-heroes/dashboard/dashboard.compone
 import { HeroDetailsComponent } from './tour-of-heroes/heroes/hero-details/hero-details.component';
 import { HeroDaoService } from './tour-of-heroes/heroes/hero-data/hero-dao.service';
 import { HeroListComponent } from './tour-of-heroes/heroes/hero-list/hero-list.component';
+import { HeroSearchComponent } from './tour-of-heroes/heroes/hero-search/hero-search.component';
 import { TourOfHeroesComponent } from './tour-of-heroes/tour-of-heroes.component';
 
 @NgModule( {
     bootstrap: [TourOfHeroesComponent],
-    declarations: [DashboardComponent, HeroDetailsComponent, HeroListComponent, TourOfHeroesComponent],
+    declarations: [DashboardComponent, HeroDetailsComponent, HeroListComponent, HeroSearchComponent, TourOfHeroesComponent],
     imports: [
         AppRoutingModule,
         BrowserModule,
