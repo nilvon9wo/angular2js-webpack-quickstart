f0865687fcd94932b9911582c770a06b57c217e8
Brian <kessler.bm@gmail.com>
2016-11-01 13:37:13 +0100

    removed unnecessary import

--- src/app/tour-of-heroes/heroes/hero-data/hero-dao.service.ts
+++ src/app/tour-of-heroes/heroes/hero-data/hero-dao.service.ts
@@ -1,4 +1,4 @@
-import { Headers, Http, Response } from '@angular/http';
+import { Headers, Http } from '@angular/http';
 import { Injectable } from '@angular/core';
 
 import 'rxjs/add/operator/toPromise';
