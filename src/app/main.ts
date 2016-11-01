e48d0b4619d4a40081aadf049ac39cfe1b12905e
Brian <kessler.bm@gmail.com>
2016-11-01 17:50:18 +0100

    changed compiler back to JiT

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,4 +1,4 @@
-import { platformBrowser } from '@angular/platform-browser';
-import { AppModuleNgFactory } from './app.module.ngfactory';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app.module';
 
-platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
+platformBrowserDynamic().bootstrapModule(AppModule);
