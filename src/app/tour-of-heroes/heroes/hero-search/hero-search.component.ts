b5c185661d89a1b09775c319f2462b902c5edd23
Brian <kessler.bm@gmail.com>
2016-11-01 17:51:11 +0100

    made heros searchable using observable

---
+++ src/app/tour-of-heroes/heroes/hero-search/hero-search.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
+
+import { Hero } from '../hero';
+import { HeroSearchService } from './hero-search.service';
+
+@Component( {
+    providers: [HeroSearchService],
+    selector: 'hero-search',
+    styles: [require( './hero-search.component.css' )],
+    template: require( './hero-search.component.html' )
+})
+
+export class HeroSearchComponent implements OnInit {
+    public heroes: Observable<Hero[]>;
+    private searchTerms = new Subject<string>();
+
+    constructor(
+        private heroSearchService: HeroSearchService,
+        private router: Router
+    ) { }
+
+    public gotoDetail(hero: Hero): void {
+        let link = ['/hero', hero.id];
+        this.router.navigate(link);
+    }
+    
+    public ngOnInit(): void {
+        this.heroes = this.searchTerms
+            .debounceTime( 300 )
+            .distinctUntilChanged()
+            .switchMap( term => term
+                ? this.heroSearchService.search( term )
+                : Observable.of<Hero[]>( [] )
+            )
+            .catch( error => {
+                console.error( error );
+                return Observable.of<Hero[]>( [] );
+            });
+    }
+
+    public search( term: string ): void {
+        this.searchTerms.next( term );
+    }
+
+}
---
+++ src/app/tour-of-heroes/heroes/hero-search/hero-search.service.ts
@@ -0,0 +1,17 @@
+import { Http, Response } from '@angular/http';
+import { Injectable } from '@angular/core';
+
+import { Observable } from 'rxjs';
+
+import { Hero } from '../hero';
+
+@Injectable()
+export class HeroSearchService {
+    constructor( private http: Http ) { }
+
+    search( term: string ): Observable<Hero[]> {
+        return this.http
+            .get( `api/heroes/?name=${term}` )
+            .map(( response: Response ) => response.json().data as Hero[] );
+    }
+}
