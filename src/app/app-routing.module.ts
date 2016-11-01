93e226f2f856f84818d2f4ee17f9cc10414ea609
Brian <kessler.bm@gmail.com>
2016-11-01 23:59:48 +0100

    reorganized routing and modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { HeroDashboardComponent } from './heroes/hero-dashboard/hero-dashboard.component';
-import { HeroDetailsComponent } from './heroes/hero-details/hero-details.component';
-import { HeroListComponent } from './heroes/hero-list/hero-list.component';
-
 const routes: Routes = [
-                        { path: '', pathMatch: 'full', redirectTo: '/dashboard' },
-                        { path: 'dashboard', component: HeroDashboardComponent  },
-                        { path: 'hero/:id', component: HeroDetailsComponent  },
-                        { path: 'heroes', component: HeroListComponent }
-                        ];
+    { path: '', pathMatch: 'full', redirectTo: 'contact' },
+    {
+        path: 'crisis-center', loadChildren: () => new Promise( resolve => {
+            ( require as any ).ensure( [], require => {
+                resolve( require( './crisis-center/crisis-center.module' ).CrisisCenterModule );
+            })
+        })
+    },
+    {
+        path: 'heroes', loadChildren: () => new Promise( resolve => {
+            ( require as any ).ensure( [], require => {
+                resolve( require( './heroes/heroes.module' ).HeroesModule );
+            })
+        })
+    }
+];
 
-@NgModule({
-    imports: [ RouterModule.forRoot(routes) ],
-    exports: [ RouterModule ]
+@NgModule( {
+    imports: [RouterModule.forRoot( routes )],
+    exports: [RouterModule]
 })
 
-export class AppRoutingModule {}
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,23 @@
+/* Angular */
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import './rxjs-extensions';
-
-import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService } from './in-memory-data.service';
-
+/* App Root */
 import { AppTitleComponent } from './app-title/app-title.component';
-import { ContactsModule } from './contacts/contacts.module';
-import { HeroesModule } from './heroes/heroes.module';
 import { HighlightDirective } from './highlight.directive';
 import { TourOfHeroesComponent } from './tour-of-heroes/tour-of-heroes.component';
 import { UserService } from './users/user.service';
 
+/* Modules */
+import { AppRoutingModule } from './app-routing.module';
+import { ContactsModule } from './contacts/contacts.module';
+
+/* Misc */
+import './rxjs-extensions';
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 @NgModule( {
     bootstrap: [TourOfHeroesComponent],
@@ -29,7 +31,6 @@ import { UserService } from './users/user.service';
         BrowserModule,
         ContactsModule,
         FormsModule,
-        HeroesModule,
         HttpModule,
         InMemoryWebApiModule.forRoot( InMemoryDataService)
     ],
