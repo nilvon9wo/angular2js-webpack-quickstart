0108bc6d2f996d8d189257ad6d5046677bdeef8f
Brian <kessler.bm@gmail.com>
2016-11-02 01:05:53 +0100

    modified highlight color and made some comments

--- src/app/shared/highlight.directive.ts
+++ src/app/shared/highlight.directive.ts
@@ -2,9 +2,16 @@ import { Directive, ElementRef, Renderer } from '@angular/core';
 
 @Directive({selector: '[highlight]'})
 
+/*
+ * TODO:
+ * app => gold
+ * contact => powderblue
+ * hero => biege
+ */
+
 export class HighlightDirective {
     constructor(renderer: Renderer, elementRef: ElementRef) {
-        renderer.setElementStyle(elementRef.nativeElement, 'backgroundColor', 'powderblue');
+        renderer.setElementStyle(elementRef.nativeElement, 'backgroundColor', 'gold'); //'powderblue', 'biege'
         console.info(`& Contact highlight called for ${elementRef.nativeElement.tagName}`);
     }
 }
