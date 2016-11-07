52aa97427fb482eb5818c54b1d33fd36b9a2b9b4
Brian <kessler.bm@gmail.com>
2016-11-07 21:05:24 +0100

    changes to whitespace

--- src/app/heroes/hero-list/hero-list.component.ts
+++ src/app/heroes/hero-list/hero-list.component.ts
@@ -4,60 +4,60 @@ import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroDaoService } from '../hero-data/hero-dao.service';
 
-@Component({
+@Component( {
     providers: [HeroDaoService],
     selector: 'hero-list',
-    styles: [require('./hero-list.component.css')],
+    styles: [require( './hero-list.component.css' )],
     template: require( './hero-list.component.html' )
 })
 
 export class HeroListComponent implements OnInit {
     public heroes: Hero[];
     public selectedHero: Hero;
-    
+
     constructor(
         private heroDaoService: HeroDaoService,
         private router: Router
     ) { }
-    
-    public add(name: String): void {
+
+    public add( name: String ): void {
         name = name.trim();
-        if (!name) {
+        if ( !name ) {
             return;
         }
-        
-        this.heroDaoService.create(name)
-            .then(hero => {
-                this.heroes.push(hero);
+
+        this.heroDaoService.create( name )
+            .then( hero => {
+                this.heroes.push( hero );
                 this.selectedHero = null;
             });
     }
-    
-    public delete(deletedHero: Hero): void {
-        this.heroDaoService.delete(deletedHero.id)
+
+    public delete( deletedHero: Hero ): void {
+        this.heroDaoService.delete( deletedHero.id )
             .then(() => {
-                this.heroes = this.heroes.filter(hero => hero !== deletedHero);
-                if (this.selectedHero === deletedHero) {
+                this.heroes = this.heroes.filter( hero => hero !== deletedHero );
+                if ( this.selectedHero === deletedHero ) {
                     this.selectedHero = null;
                 }
             });
     }
-    
+
     public gotoDetail( hero: Hero ): void {
         const heroDetailsById = ['/hero', this.selectedHero.id];
         this.router.navigate( heroDetailsById );
     }
-    
+
     public initHeroes(): void {
         this.heroDaoService.getHeroes()
-            .then(heroes => this.heroes = heroes);
+            .then( heroes => this.heroes = heroes );
     }
-    
+
     public ngOnInit(): void {
         this.initHeroes();
     }
-    
-    public onSelect(hero: Hero): void {
+
+    public onSelect( hero: Hero ): void {
         this.selectedHero = hero;
     }
 }
